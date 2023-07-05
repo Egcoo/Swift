b587ca981a66f3fc995b56b05605fcfc3eb981ce
Egcoo <2957448049@qq.com>
2023-07-06 01:05:39 +0800

    first update

---
+++ SwiftBase/SwiftBase01/swift101/swift101.swift
@@ -0,0 +1,13 @@
+// 变量 = 关键字 + 变量名称 ： 类型  = 值
+
+let number: Int = 10
+
+print(number)
+print("Hello, World!")
+
+
+let name = "Egcoo"
+let location = "Tokyo"
+
+
+print("Hello", name, "from", location)
